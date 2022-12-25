d922088f4b4bc2486f1fa701006c7a5223b69ac9
LENOVO <LENOVO@DESKTOP-J080GV5>
2022-12-25 22:41:34 +0300

    deneme

--- src/View/DoktorLogin.java
+++ src/View/DoktorLogin.java
@@ -31,7 +31,7 @@ public class DoktorLogin extends JFrame {
 	 */
 	public DoktorLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 460, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
