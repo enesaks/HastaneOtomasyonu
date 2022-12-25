0adc9adaa43059fa939c2565b0efb3064f2f40cb
BUGRA <BUGRA@DESKTOP-MOCHFHD>
2022-12-26 02:18:33 +0300

    HastaLogin()class'ında kodlar yazıldı..

--- src/View/HastaLogin.java
+++ src/View/HastaLogin.java
@@ -5,18 +5,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import DataBase.Hasta;
+import Helper.DBConnection;
+import Helper.Helper;
+
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
 
 public class HastaLogin extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField fldTCHastaGiris;
-	private JTextField fldSifreHastaGiris;
-	private JPasswordField fldGogumTarihiHastaGiris;
+	private JPasswordField fldSifreHastaGiris;
+	private DBConnection conn = new DBConnection();
 
 	/**
 	 * Launch the application.
@@ -45,44 +58,92 @@ public class HastaLogin extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lbl_TCHastaGiris = new JLabel("tc");
-		lbl_TCHastaGiris.setBounds(76, 74, 46, 14);
+
+		JLabel lbl_TCHastaGiris = new JLabel("T.C Numaranız");
+		lbl_TCHastaGiris.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		lbl_TCHastaGiris.setBounds(76, 85, 113, 17);
 		contentPane.add(lbl_TCHastaGiris);
-		
-		JLabel lblSifreHastaGiris = new JLabel("sifre");
-		lblSifreHastaGiris.setBounds(76, 108, 46, 14);
+
+		JLabel lblSifreHastaGiris = new JLabel("SİFRE");
+		lblSifreHastaGiris.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		lblSifreHastaGiris.setBounds(76, 124, 99, 14);
 		contentPane.add(lblSifreHastaGiris);
-		
-		JLabel lblDogumtarihiHastaGiris = new JLabel("dg");
-		lblDogumtarihiHastaGiris.setBounds(76, 150, 46, 14);
-		contentPane.add(lblDogumtarihiHastaGiris);
-		
-		JButton butonGirisHastaGiris = new JButton("giris");
-		butonGirisHastaGiris.setBounds(100, 210, 89, 23);
+
+		JButton butonGirisHastaGiris = new JButton("Giriş Yap");
+		butonGirisHastaGiris.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		butonGirisHastaGiris.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (fldTCHastaGiris.getText().length() == 0 || fldSifreHastaGiris.getText().length() == 0) {
+					Helper.showMsg("fill");
+				} else {
+					boolean key = true;
+					try {
+						Connection con = conn.connDb();
+						Statement st = con.createStatement();
+						ResultSet rs = st.executeQuery("SELECT * FROM user");
+
+						while (rs.next()) {
+							if (fldTCHastaGiris.getText().equals(rs.getString("tcno"))
+									&& fldSifreHastaGiris.getText().equals(rs.getString("sifre"))) {
+
+								if (rs.getString("type").equals("hasta")) {
+									Hasta hasta = new Hasta();
+									hasta.setId(rs.getInt("id"));
+									hasta.setAd(rs.getString("ad"));
+									hasta.setSoyad(rs.getString("soyad"));
+									hasta.setTcno(rs.getString("tcno"));
+									hasta.setSifre(rs.getString("sifre"));
+									hasta.setType(rs.getString("type"));
+									System.out.println(hasta.getAd());
+									HastaGenel hGenel = new HastaGenel();
+									// DÜZELTİLECEK
+									hGenel.setVisible(true);
+									// Diger panele taşımayı sağlar..
+									dispose();
+									// Önceki paneli kapatır..
+									key = false;
+								}
+							}
+						}
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					if (key) {
+						Helper.showMsg("Böyle bir hasta bulunamadı lütfen kayıt olunuz");
+					}
+				}
+			}
+		});
+		butonGirisHastaGiris.setBounds(76, 187, 113, 46);
 		contentPane.add(butonGirisHastaGiris);
-		
-		JButton butonKayitOlHastaGiris = new JButton("kayit ol");
-		butonKayitOlHastaGiris.setBounds(243, 210, 89, 23);
+
+		JButton butonKayitOlHastaGiris = new JButton("Kayıt Ol");
+		butonKayitOlHastaGiris.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		butonKayitOlHastaGiris.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				HastaKayıtOl hKayıt=new HastaKayıtOl();
+				hKayıt.setVisible(true);
+				dispose();				
+			}
+		});
+		butonKayitOlHastaGiris.setBounds(243, 187, 113, 46);
 		contentPane.add(butonKayitOlHastaGiris);
-		
+
 		fldTCHastaGiris = new JTextField();
-		fldTCHastaGiris.setBounds(243, 71, 86, 20);
+		fldTCHastaGiris.setBounds(243, 86, 99, 20);
 		contentPane.add(fldTCHastaGiris);
 		fldTCHastaGiris.setColumns(10);
-		
-		fldSifreHastaGiris = new JTextField();
-		fldSifreHastaGiris.setBounds(246, 105, 86, 20);
-		contentPane.add(fldSifreHastaGiris);
-		fldSifreHastaGiris.setColumns(10);
-		
-		fldGogumTarihiHastaGiris = new JPasswordField();
-		fldGogumTarihiHastaGiris.setBounds(243, 147, 89, 20);
-		contentPane.add(fldGogumTarihiHastaGiris);
-		
-		JLabel baslik = new JLabel("hasta giris ekrani");
-		baslik.setBounds(76, 27, 165, 14);
-		contentPane.add(baslik);
-	}
 
+		JLabel baslik = new JLabel("Hasta Giris Ekranı");
+		baslik.setForeground(Color.RED);
+		baslik.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
+		baslik.setBounds(140, 28, 175, 20);
+		contentPane.add(baslik);
+
+		fldSifreHastaGiris = new JPasswordField();
+		fldSifreHastaGiris.setBounds(243, 124, 99, 19);
+		contentPane.add(fldSifreHastaGiris);
+	}
 }
