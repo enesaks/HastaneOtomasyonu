503a4b0106e3c3161a85a3f247d675f566e110fa
enesc <enesc@enes-pc>
2022-12-25 20:16:25 +0300

    Databese bağlantısı yapıldı(DBconnectindan)

--- src/Helper/DBConnection.java
+++ src/Helper/DBConnection.java
@@ -1,5 +1,24 @@
 package Helper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+Connection c = null;
+	
+	public DBConnection() {
+		
+	}
+	
+	public Connection connDb() {
+		try {
+			this.c = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital?user=root&password=root");
+			return c;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
 
 }
