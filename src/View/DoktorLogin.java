9d80816bb568b1ee62aa71de08014159e8c7121d
zeyne <zeyne@zeynep>
2022-12-28 00:39:16 +0300

    doktor girişe geri butonu eklendi

--- src/View/DoktorLogin.java
+++ src/View/DoktorLogin.java
@@ -12,8 +12,10 @@ import Helper.Helper;
 import DataBase.Bashekim;
 
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -60,16 +62,17 @@ public class DoktorLogin extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lbl_TC = new JLabel("TC KİMLİK NO:\r\n");
-		lbl_TC.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
 		lbl_TC.setBounds(74, 95, 128, 23);
+		lbl_TC.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
 		contentPane.add(lbl_TC);
 		
 		JLabel lblSifre = new JLabel("ŞİFRE:");
-		lblSifre.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
 		lblSifre.setBounds(74, 140, 113, 14);
+		lblSifre.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
 		contentPane.add(lblSifre);
 		
 		JButton butonGiris = new JButton("GİRİŞ");
+		butonGiris.setBounds(160, 203, 99, 23);
 		butonGiris.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
 		butonGiris.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -135,7 +138,6 @@ public class DoktorLogin extends JFrame {
 				
 			
 		});
-		butonGiris.setBounds(160, 203, 99, 23);
 		contentPane.add(butonGiris);
 		
 		fldTC = new JTextField();
@@ -148,9 +150,28 @@ public class DoktorLogin extends JFrame {
 		contentPane.add(fldSifre);
 		
 		JLabel baslik = new JLabel("DOKTOR GİRİŞİ");
+		baslik.setBounds(161, 24, 113, 31);
 		baslik.setForeground(Color.BLUE);
 		baslik.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		baslik.setBounds(161, 24, 113, 31);
 		contentPane.add(baslik);
+		
+		JButton geri_buton = new JButton(new ImageIcon(this.getClass().getResource("return.png")));
+		geri_buton.setBorder(UIManager.getBorder("Tree.editorBorder"));
+		geri_buton.setBounds(10, 11, 34, 31);
+		contentPane.add(geri_buton);
+		
+		
+		
+		geri_buton.setBackground(Color.LIGHT_GRAY);
+		
+		
+		geri_buton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		
+		   
+		
+		});
 	}
 }
