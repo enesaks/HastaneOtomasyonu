97d12acd4715250746ce71d8ef8488fd88aaf7a7
zeyne <zeyne@zeynep>
2022-12-27 22:43:51 +0300

    hasta logine foto eklenecek o hariç kodu yazıldı

--- src/View/SecimLogin.java
+++ src/View/SecimLogin.java
@@ -34,7 +34,7 @@ public class SecimLogin extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public SecimLogin() {
+	public SecimLogin() { 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
