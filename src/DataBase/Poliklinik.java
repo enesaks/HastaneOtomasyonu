7778784c4ebfa4f5f44b1095348e98acec472948
BUGRA <BUGRA@DESKTOP-MOCHFHD>
2022-12-28 19:27:48 +0300

    poliklinik

--- src/DataBase/Poliklinik.java
+++ src/DataBase/Poliklinik.java
@@ -1,10 +1,74 @@
 package DataBase;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import Helper.DBConnection;
+
 public class Poliklinik {
 
-private String polikliniIsmi;
-	
+	private String polikliniIsmi;
+	private int id;
+	private String name;
+	DBConnection conn = new DBConnection();
+
+	Statement st = null;
+	ResultSet rs = null;
+	PreparedStatement preparedStatement = null;
+
+	public Poliklinik(String polikliniIsmi, int id, String name) {
+		super();
+		this.polikliniIsmi = polikliniIsmi;
+		this.id = id;
+		this.name = name;
+	}
+
 	public Poliklinik() {
+	}
+
+	public ArrayList<Poliklinik> getList() throws SQLException {
+		ArrayList<Poliklinik> list = new ArrayList<>();
+		Poliklinik obj;
+		Connection con = conn.connDb();
+		try {
+
+			st = con.createStatement();
+			rs = st.executeQuery("SELECT * FROM clinic");
+			while (rs.next()) {
+				obj = new Poliklinik();
+				obj.setId(rs.getInt("id"));
+				obj.setName(rs.getString("name"));
+				list.add(obj);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			st.close();
+			rs.close();
+			con.close();
+		}
+		return list;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getPolikliniIsmi() {
@@ -13,6 +77,5 @@ private String polikliniIsmi;
 
 	public void setPolikliniIsmi(String polikliniIsmi) {
 		this.polikliniIsmi = polikliniIsmi;
-	}  	 
+	}
 }
- 
