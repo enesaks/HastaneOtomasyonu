9c6032acf127c45182f92c6683be8b2c383cba3e
BUGRA <BUGRA@DESKTOP-MOCHFHD>
2022-12-28 03:39:48 +0300

    ekleme boş

--- src/View/CalısmaSaatleri.java
+++ src/View/CalısmaSaatleri.java
@@ -6,6 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class CalısmaSaatleri extends JFrame {
 
@@ -32,11 +35,22 @@ public class CalısmaSaatleri extends JFrame {
 	 */
 	public CalısmaSaatleri() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 650, 473);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(23, 59, 603, 347);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Çalışma Saatleri");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 20));
+		lblNewLabel.setBounds(23, 10, 291, 39);
+		contentPane.add(lblNewLabel);
 	}
 }
